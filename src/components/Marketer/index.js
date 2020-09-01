382d8ab26cdd49f1b9ac2ae1341f5044087d035f
Sergey Rudenko <sergey.rudenko@intspirit.com>
2020-09-01 11:47:18 +0300

    Add comments count

--- src/components/Marketer/index.js
+++ src/components/Marketer/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import dateFormat from "dateformat";
 import {Line, Pie, Bar} from 'react-chartjs-2';
 import './styles.scss'
 
 const state = {
   labels: ['January', 'February', 'March',
-    'April', 'May'],
+    'April', 'May', 'title'],
   datasets: [
     {
-      label: 'Rainfall',
       backgroundColor: [
         '#B21F00',
         '#C9DE00',
@@ -23,21 +24,76 @@ const state = {
         '#35014F'
       ],
       lineTension: 0.5,
-      borderColor: 'rgba(0,128,0,1)',
+      borderColor: 'rgba(0,0,0,1)',
       borderWidth: 1,
-      data: [65, 59, 80, 81, 56]
+      data: [65, 59, 80, 81, 56, 70]
     }
   ]
 }
-
+const getData = (link) => axios.get(link);
+const getCommentsCount = (data) => {
+  const authors = new Set(data.map(item => item.username))
+  const commentCounter = []
+  authors.forEach(item => {
+    commentCounter.push(data.filter(d => d.username === item).length)
+  })
+  return {authors: [...authors], counter: commentCounter}
+}
+const addCommentsDataset = (state, counters, users) => {
+  const newState = Object.assign({}, state);
+  newState.labels = users;
+  newState.datasets[0].data = counters
+  return newState
+}
 export default function Marketer() {
+  const [post, setPost] = useState([]);
+  const [commentsCount, setCommentsCount] = useState([]);
+  const [userComments, setUserComments] = useState([]);
+  useEffect(() => {
+    getData("http://52.175.201.248:3000/facebook/facebook_post/1")
+      .then((response) => {
+        const comments = getCommentsCount(response.data.list)
+        setUserComments(comments.authors)
+        setCommentsCount(comments.counter)
+        setPost(response.data.list.slice(0, 5))
+      })
+  }, []);
   return (
     <div className="marketer">
       <div className="marketer__select">Select</div>
-      <div className="marketer__graph">Table</div>
+      <div className="marketer__graph">
+        {
+          post.length
+            ? <table className="marketer__table">
+              <thead>
+              <tr className="marketer__tr marketer__tr_header">
+                <td className="marketer__cell">Created</td>
+                <td className="marketer__cell">Post text</td>
+                <td className="marketer__cell">Likes</td>
+                <td className="marketer__cell">Author</td>
+              </tr>
+              </thead>
+              <tbody>
+              {
+                post.map(
+                  ({post_id, created_datetime, post, post_likes, username}, index) =>
+                    <tr key={post_id}
+                        className={`marketer__tr ${index % 2 !== 0 ? "marketer__tr_lightgreen" : ""}`}>
+                      <td className="marketer__cell">{dateFormat(created_datetime, "dd.mm.yyyy HH:MM")}</td>
+                      <td className="marketer__cell">{post}</td>
+                      <td className="marketer__cell">{post_likes}</td>
+                      <td className="marketer__cell"> {username}</td>
+                    </tr>)
+              }
+              </tbody>
+            </table>
+            : null
+        }
+
+      </div>
       <div className="marketer__graph">
         <Bar
-          data={state}
+          data={addCommentsDataset(state, commentsCount, userComments)}
           width={600}
           height={300}
           options={{
@@ -46,7 +102,9 @@ export default function Marketer() {
               text: 'Comments count',
               fontSize: 18
             },
-
+            legend: {
+              display: false
+            }
           }}
         />
       </div>
@@ -61,7 +119,9 @@ export default function Marketer() {
               text: 'Daily comments',
               fontSize: 18
             },
-
+            legend: {
+            display: false
+          }
           }}
         /></div>
       <div className="marketer__graph">
@@ -75,7 +135,9 @@ export default function Marketer() {
               text: '% Likes',
               fontSize: 18
             },
-
+            legend: {
+              display: false
+            }
           }}
         /></div>
     </div>
